0d21b011a676d703cb38caeaa8850f43be832d27
LucaSpennato <luca.spennato@gmail.com>
2022-07-04 17:41:00 +0200

    bonus

--- js/main.js
+++ js/main.js
@@ -2,8 +2,34 @@ const app = new Vue(
     {
         el: '#root',
         data: {
-            input:'',
-            heading: 'Hello! ',
-        }
+            title: 'Compila la tua card!',
+            heading: 'Henlo ',
+            userInput: '',
+            userInputSent: '',
+            content: '',
+            image: '',
+            imageSrc: '',
+            contentSaved: '',
+        },
+
+        methods: {
+
+            cardCompiler: function(){
+
+                this.userInputSent = this.userInput;
+                this.userInput = '';
+            
+                this.contentSaved = this.content;
+                this.content = '';
+
+                this.imageSrc = this.image;
+                this.image = '';
+
+                if((this.userInputSent !== '') && (this.contentSaved !== '') && (this.imageSrc !== '')){
+                    this.title = 'Card compilata con successo!';
+                }
+            }
+
+        },
     }
-);
+)
